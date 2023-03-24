72bffd83715cf2e0d9d7fcf7f6d3c4b9fb68e856
nifty_afk <nifty_afk@yahoo.com>
2023-03-24 21:20:20 +0100

    configuration management install package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,12 @@
 # install puppet-lint -v 2.1.0
 
-exec { 'puppet-lint':
-  command => '/usr/bin/apt-get -y install puppet-lint -v 2.1.0',
+#exec { 'puppet-lint':
+#  command => '/usr/bin/apt-get -y install puppet-lint -v 2.1.0',
+#}
+
+
+# installing a package using puppet
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
